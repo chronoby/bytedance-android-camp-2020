6e71fb282bf6ed4d49d047ce16c283599c3e05ca
chronoby <1076947334@qq.com>
2020-07-15 01:02:35 +0800

    Chapter 7

---
+++ chapter7/app/src/main/java/com/domker/study/androidstudy/StudyApplication.java
@@ -0,0 +1,11 @@
+package com.domker.study.androidstudy;
+
+import android.app.Application;
+
+public class StudyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
